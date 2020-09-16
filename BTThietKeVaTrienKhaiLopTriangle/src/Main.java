1e6a5ac5a79ece2895827d094c7af026421faa3c
omgbqh98 <69282402+omgbqh98@users.noreply.github.com>
2020-09-16 19:59:14 +0700

    triangle

---
+++ BTThietKeVaTrienKhaiLopTriangle/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Triangle triangle = new Triangle("yellow",true,4,6,2);
+        System.out.println(triangle.toString());
+    }
+}
